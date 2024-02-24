51c53c21d349749461900d858d4fe4bd8c2bbf03
cristinedior <148567509+cristinedior@users.noreply.github.com>
2024-02-24 12:12:54 -0600

    added rest of working html and script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,17 @@
 $(function () {
-  // Display the current date in the page header
+  // Display the current day at the top of the calendar
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 
-  // Function to update the time block classes according to the current time
+  // Function to color-code each time block to indicate whether it's in the past, present, or future
   function updateTimeBlocks() {
-    var currentHour = dayjs().hour(); // Get the current hour using dayjs
+    var currentHour = dayjs().hour(); // Get the current hour using Day.js
 
-    // Loop over each time block
+    // Loop over each time block and update its class based on the current time
     $('.time-block').each(function () {
       var blockHour = parseInt($(this).attr('id').replace('hour-', ''), 10);
-
-      // Remove any old classes from the block
       $(this).removeClass('past present future');
 
-      // Add the appropriate class based on the current time
+      // Apply color coding: past, present, future
       if (blockHour < currentHour) {
         $(this).addClass('past');
       } else if (blockHour === currentHour) {
@@ -24,25 +22,21 @@ $(function () {
     });
   }
 
-  // Load any saved data from localStorage and update the time block classes
+  // Load any saved events from localStorage and populate the time blocks
   $('.time-block').each(function () {
     var hourId = $(this).attr('id');
     var savedText = localStorage.getItem(hourId);
-
-    // Set the text to the textarea
     $(this).find('.description').val(savedText);
   });
 
-  // Update the time blocks on initial load and then every 5 minutes to reflect the current time
+  // Update the time block classes on initial load and then every 5 minutes
   updateTimeBlocks();
   setInterval(updateTimeBlocks, 300000); // 300000 milliseconds = 5 minutes
 
-  // Click event listener for save buttons
+  // Event listener for save buttons to save the corresponding event in local storage
   $('.saveBtn').click(function () {
     var hourId = $(this).closest('.time-block').attr('id');
     var text = $(this).siblings('.description').val();
-
-    // Save the text in local storage
     localStorage.setItem(hourId, text);
   });
 });
